7c0519293b7e4136c2f9a85e6412a171c6db5c9d
Eric Espie <eric.espie@combodo.com>
2022-10-07 17:36:55 +0200

    N°5400 - Fix unit tests

--- test/Service/TimeRangeWeeklyScheduledServiceTest.php
+++ test/Service/TimeRangeWeeklyScheduledServiceTest.php
@@ -8,14 +8,15 @@ namespace Combodo\iTop\ComplexBackgroundTask\Test\Service;
 
 use Combodo\iTop\ComplexBackgroundTask\Helper\ComplexBackgroundTaskLog;
 use Combodo\iTop\ComplexBackgroundTask\Service\TimeRangeWeeklyScheduledService;
-use Combodo\iTop\Test\UnitTest\ItopDataTestCase;
+use Combodo\iTop\Test\UnitTest\ItopTestCase;
 use DateTime;
 
-class TimeRangeWeeklyScheduledServiceTest extends ItopDataTestCase
+class TimeRangeWeeklyScheduledServiceTest extends ItopTestCase
 {
 	protected function setUp(): void
 	{
 		parent::setUp();
+		require_once (__DIR__.'/../../vendor/autoload.php');
 		date_default_timezone_set('UTC');
 		$this->TEST_LOG_FILE = APPROOT.'log/test.log';
 		ComplexBackgroundTaskLog::Enable($this->TEST_LOG_FILE);
@@ -34,7 +35,7 @@ class TimeRangeWeeklyScheduledServiceTest extends ItopDataTestCase
 	/**
 	 * @dataProvider GetNextOccurrenceProvider
 	 *
-	 * @param $dExpected
+	 * @param $sExpected
 	 * @param $bEnabled
 	 * @param $sStartTime
 	 * @param $sEndTime
@@ -45,17 +46,21 @@ class TimeRangeWeeklyScheduledServiceTest extends ItopDataTestCase
 	 * @return void
 	 * @throws \Combodo\iTop\ComplexBackgroundTask\Helper\ComplexBackgroundTaskException
 	 */
-	public function testGetNextOccurrence($dExpected, $bEnabled, $sStartTime, $sEndTime, $sTimeLimit, $iCurrentTime, $aDays)
+	public function testGetNextOccurrence($sExpected, $bEnabled, $sStartTime, $sEndTime, $sTimeLimit, $sCurrentTime, $aDays)
 	{
 		$oAnonymizerService = new TimeRangeWeeklyScheduledService();
-		$this->assertEquals($dExpected, $oAnonymizerService->GetNextOccurrence($bEnabled, $sStartTime, $sEndTime, $sTimeLimit, $iCurrentTime, $aDays));
+		$iTimeLimit = date_format(date_create( $sTimeLimit), 'U');
+		$iCurrentTime = date_format(date_create($sCurrentTime), 'U');
+		$oExpected = new DateTime($sExpected);
+		$this->assertEquals($oExpected, $oAnonymizerService->GetNextOccurrence($bEnabled, $sStartTime, $sEndTime, $iTimeLimit, $iCurrentTime, $aDays));
 	}
 
 	public function GetNextOccurrenceProvider()
 	{
+		// 2022-10-07 is a friday
 		return [
-			'disabled' => [new DateTime('3000-01-01'), false, '00:30', '05:30', 1700000000, 1664464807, [1,2,3,4,5,6,7]],
-			'next 2s' => [new DateTime('3000-01-01'), false, '00:30', '05:30', 1700000000, 1664464807, [1,2,3,4,5,6,7]],
+			'disabled' => ['3000-01-01', false, '00:30', '05:30', '2022-10-07 17:30', '2022-10-07 17:00', [1,2,3,4,5,6,7]],
+			'next 2s' => ['2022-10-08T00:30:00', true, '00:30', '05:30', '2022-10-07 17:30', '2022-10-07 17:00', [1,2,3,4,5,6,7]],
 		];
 	}
 }
