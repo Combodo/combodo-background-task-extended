c04b0e9c5556bce04d860f2b246c8f29c13eae5d
Amine <amine.ait-ali@etu.univ-grenoble-alpes.fr>
2026-02-17 15:56:05 +0100

    1.0.3

--- module.combodo-background-task-extended.php
+++ module.combodo-background-task-extended.php
@@ -6,7 +6,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-background-task-extended/1.0.2',
+	'combodo-background-task-extended/1.0.3',
 	[
 		// Identification
 		//
